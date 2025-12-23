28c278c6c64624d14c0c591013dfb9a037be3344
ddubuk228 <practice2280@gmail.com>
2025-12-23 16:28:00 +0900

    0007

--- src/main/java/com/back/boundedContext/post/app/PostService.java
+++ src/main/java/com/back/boundedContext/post/app/PostService.java
@@ -1,8 +1,8 @@
-package com.back.boundedContext.post.service;
+package com.back.boundedContext.post.app;
 
-import com.back.boundedContext.member.entity.Member;
-import com.back.boundedContext.post.entity.Post;
-import com.back.boundedContext.post.repository.PostRepository;
+import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.post.domain.Post;
+import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
@@ -24,11 +24,7 @@ public class PostService {
     public Post write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
-        eventPublisher.publish(
-                new PostCreatedEvent(
-                        new PostDto(post)
-                )
-        );
+        eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
 
         //author.increaseActivityScore(3);
         return post;
