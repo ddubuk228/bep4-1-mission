3941e81e24ba275693978d005dd748762def4fe5
ddubuk228 <practice2280@gmail.com>
2025-12-24 10:22:37 +0900

    0009

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -4,33 +4,17 @@ import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
-import com.back.shared.post.dto.PostDto;
-import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
-public class PostService {
+public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
 
-    public long count() {
-        return postRepository.count();
-    }
-
     public Post write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
-
-        eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
-
-        //author.increaseActivityScore(3);
         return post;
-    }
-
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
     }
 }
